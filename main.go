b0635205bed09bca9afd3912044827ffba26e643
denysrondaliev <denysrondaliev@gmail.com>
2024-08-18 16:38:13 +0300

    feat: added getPrivateIP func

--- main.go
+++ main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	"fmt"
+	"net"
 	"net/http"
 	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
+
+func getPrivateIP() (string, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+
+	for _, addr := range addrs {
+		// Check if the address is a valid IP address and is not a loopback
+		ipNet, ok := addr.(*net.IPNet)
+		if ok && !ipNet.IP.IsLoopback() {
+			// Check for IPv4 addresses only
+			if ipNet.IP.To4() != nil {
+				return ipNet.IP.String(), nil
+			}
+		}
+	}
+
+	return "", fmt.Errorf("no private IP address found")
+}
 
 func main() {
 
@@ -16,7 +38,11 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello, Docker! <3")
+		privateIP, err := getPrivateIP()
+		if err != nil {
+			return c.HTML(http.StatusInternalServerError, "Failed to retrieve IP address")
+		}
+		return c.HTML(http.StatusOK, fmt.Sprintf("Hello, my  private IP is %s", privateIP))
 	})
 
 	e.GET("/health", func(c echo.Context) error {
